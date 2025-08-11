5ee09c27acfd3b9c70c02404e7e008479b20d671
ahmadha1 <ahmadha74@gmail.com>
2025-08-10 17:04:01 -0700

    Initial Commit

---
+++ Remember/RememberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RememberApp.swift
+//  Remember
+//
+//  Created by Ahmad Hamilton on 8/10/25.
+//
+
+import SwiftUI
+
+@main
+struct RememberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
